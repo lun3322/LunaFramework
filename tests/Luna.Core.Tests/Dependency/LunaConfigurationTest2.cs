cdfab5b2e900a6dfaa06d5db988c204cd0847295
lun3322 <lun3322@126.com>
2018-12-26 23:45:22 +0800

    添加一个单元测试

---
+++ tests/Luna.Core.Tests/Dependency/LunaConfigurationTest2.cs
@@ -0,0 +1,15 @@
+﻿using Luna.Dependency;
+
+namespace Luna.Core.Tests.Dependency
+{
+    public class LunaConfigurationTest2 : LunaConfiguration
+    {
+        public override void Initialize()
+        {
+        }
+
+        public override void Setup()
+        {
+        }
+    }
+}
---
+++ tests/Luna.Core.Tests/Dependency/LunaConfigurationTests.cs
@@ -0,0 +1,20 @@
+﻿using Luna.Dependency;
+using Shouldly;
+using Xunit;
+
+namespace Luna.Core.Tests.Dependency
+{
+    public class LunaConfigurationTests : TestBase
+    {
+        [Fact]
+        public void Multi_LunaConfiguration_Test()
+        {
+            var resolveAll = IocManager.ResolveAll<ILunaConfiguration>();
+
+            resolveAll.Length.ShouldBeGreaterThan(1);
+
+            resolveAll.ShouldContain(m => m.GetType() == typeof(LunaConfigurationTest1));
+            resolveAll.ShouldContain(m => m.GetType() == typeof(LunaConfigurationTest2));
+        }
+    }
+}
