1ee314076d09693218785beaaebd98a75109d040
lun3322 <lun3322@126.com>
2019-01-07 22:30:43 +0800

    更新打包

--- buildpackage.ps1
+++ buildpackage.ps1
@@ -1,3 +1,4 @@
 dotnet pack .\src\Luna.Core\Luna.Core.csproj -c Release
 dotnet pack .\src\Luna.Castle.Nlog\Luna.Castle.Nlog.csproj -c Release
 dotnet pack .\src\Luna.Web\Luna.Web.csproj -c Release
+dotnet pack .\src\Luna.Dapper\Luna.Dapper.csproj -c Release
