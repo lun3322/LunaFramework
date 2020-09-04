82324a610d1c474d2fdbac2d8919f16a90fc92da
hong <lun3322@126.com>
2020-09-04 16:32:31 +0800

    更新到0.6.4 增加一种分页方法

--- src/Luna.Dapper/PagedExtension.cs
+++ src/Luna.Dapper/PagedExtension.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Dapper;
 using Luna.Application.Dto;
 
@@ -12,6 +13,8 @@ namespace Luna.Dapper
     /// </summary>
     public static class PagedExtension
     {
+        private static readonly Regex REPLACE_SELECT_SQL_TO_COUNT_SQL = new Regex("SELECT.+?FROM", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
         /// <summary>
         /// 分页方法
         /// </summary>
@@ -42,6 +45,38 @@ namespace Luna.Dapper
         }
 
         /// <summary>
+        /// 分页方法
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="selectSql"></param>
+        /// <param name="sortField"></param>
+        /// <param name="param"></param>
+        /// <param name="transaction"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static (List<T> data, int count) GetPagedList<T>(this IDbConnection connection, string selectSql, string sortField,
+            object param, IDbTransaction transaction = null)
+        {
+            if (!(param is RequestPagedVm))
+            {
+                throw new Exception($"{nameof(param)}参数类型错误");
+            }
+
+            var countSql = REPLACE_SELECT_SQL_TO_COUNT_SQL.Replace(selectSql, "SELECT COUNT(1) FROM");
+
+            var sql = $@"
+{GetPagedSql(selectSql, sortField)}
+{countSql}
+";
+            using var reader = connection.QueryMultiple(sql, param, transaction);
+            var list = reader.Read<T>().ToList();
+            var count = reader.ReadSingle<int>();
+            return (list, count);
+        }
+
+
+        /// <summary>
         /// 总数sql
         /// </summary>
         /// <param name="conditionSql"></param>
@@ -65,6 +100,20 @@ SELECT COUNT(1)
             return $@"
 {selectSql}
 {conditionSql}
+ORDER BY {sortField}
+OFFSET (@pageIndex - 1) * @pageSize ROWS FETCH NEXT @pageSize ROWS ONLY;";
+        }
+
+        /// <summary>
+        /// 分页语句
+        /// </summary>
+        /// <param name="selectSql"></param>
+        /// <param name="sortField"></param>
+        /// <returns></returns>
+        public static string GetPagedSql(string selectSql, string sortField)
+        {
+            return $@"
+{selectSql}
 ORDER BY {sortField}
 OFFSET (@pageIndex - 1) * @pageSize ROWS FETCH NEXT @pageSize ROWS ONLY;";
         }
