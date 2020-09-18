2e8f713eaf21d0f55c22fd4a23ffe6e85cde0ae3
hong <lun3322@126.com>
2020-09-18 15:14:18 +0800

    add publish shell

---
+++ publish2Nuget.sh
@@ -0,0 +1,9 @@
+dotnet pack src/Luna.Core/Luna.Core.csproj -c Release
+dotnet pack src/Luna.Castle.Nlog/Luna.Castle.Nlog.csproj -c Release
+dotnet pack src/Luna.Web/Luna.Web.csproj -c Release
+dotnet pack src/Luna.Dapper/Luna.Dapper.csproj -c Release
+
+for line in `ls package/*`
+do
+        dotnet nuget push $line -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
+done
