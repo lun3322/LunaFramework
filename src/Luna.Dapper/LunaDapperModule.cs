0ea62ab88a3583ace66df2c424a0f114caea6c28
hong <lun3322@126.com>
2020-09-25 17:39:21 +0800

    添加两个模块类

---
+++ src/Luna.Dapper/LunaDapperModule.cs
@@ -0,0 +1,13 @@
+﻿using Luna.Dependency;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Luna.Dapper
+{
+    [Dependency(typeof(LunaCoreModule))]
+    public class LunaDapperModule : LunaModule
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+    }
+}
---
+++ src/Luna.Web/LunaWebModule.cs
@@ -0,0 +1,13 @@
+﻿using Luna.Dependency;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Luna.Web
+{
+    [Dependency(typeof(LunaCoreModule))]
+    public class LunaWebModule : LunaModule
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+    }
+}
