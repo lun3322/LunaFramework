34688101178b2fd0caf94401cf13138a08272b68
hong <lun3322@126.com>
2020-09-27 16:35:47 +0800

    更新项目文件和发布脚本

--- buildpackage.bat
+++ buildpackage.bat
@@ -1,3 +1,4 @@
 dotnet pack .\src\Luna.Core\Luna.Core.csproj -c Release
 dotnet pack .\src\Luna.Web\Luna.Web.csproj -c Release
 dotnet pack .\src\Luna.Dapper\Luna.Dapper.csproj -c Release
+dotnet pack .\src\Luna.MongoDb\Luna.MongoDb.csproj -c Release
--- publish2Nuget.sh
+++ publish2Nuget.sh
@@ -1,6 +1,7 @@
 dotnet pack src/Luna.Core/Luna.Core.csproj -c Release
 dotnet pack src/Luna.Web/Luna.Web.csproj -c Release
 dotnet pack src/Luna.Dapper/Luna.Dapper.csproj -c Release
+dotnet pack src/Luna.MongoDb/Luna.MongoDb.csproj -c Release
 
 for line in `ls package/*`
 do
