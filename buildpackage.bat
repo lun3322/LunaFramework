833b31bb5f96bfdd61ee61689869462fd3133012
hong <lun3322@126.com>
2020-09-29 14:41:42 +0800

    添加 NLog 支持

--- buildpackage.bat
+++ buildpackage.bat
@@ -3,3 +3,4 @@ dotnet pack .\src\Luna.Web\Luna.Web.csproj -c Release
 dotnet pack .\src\Luna.Dapper\Luna.Dapper.csproj -c Release
 dotnet pack .\src\Luna.MongoDb\Luna.MongoDb.csproj -c Release
 dotnet pack .\src\Luna.Redis.AspNetCore\Luna.Redis.AspNetCore.csproj -c Release
+dotnet pack .\src\Luna.Nlog.AspNetCore\Luna.Nlog.AspNetCore.csproj -c Release
--- publish2Nuget.sh
+++ publish2Nuget.sh
@@ -3,6 +3,7 @@ dotnet pack src/Luna.Web/Luna.Web.csproj -c Release
 dotnet pack src/Luna.Dapper/Luna.Dapper.csproj -c Release
 dotnet pack src/Luna.MongoDb/Luna.MongoDb.csproj -c Release
 dotnet pack src/Luna.Redis.AspNetCore/Luna.Redis.AspNetCore.csproj -c Release
+dotnet pack src/Luna.Nlog.AspNetCore/Luna.Nlog.AspNetCore.csproj -c Release
 
 for line in `ls package/*`
 do
